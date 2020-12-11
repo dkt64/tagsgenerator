d3ec4c89224fd091f25f4f1fb3f51c452f6419f6
DKT <b.apanasewicz@gmail.com>
2020-12-11 13:27:01 +0100

    cross-build

---
+++ build.bat
@@ -0,0 +1,12 @@
+set GOOS=windows
+set GOARCH=amd64
+go build -o bin/win64/tagsgenerator.exe
+set GOOS=linux
+set GOARCH=amd64
+go build -o bin/lin64/tagsgenerator
+set GOOS=linux
+set GOARCH=arm64
+go build -o bin/arm64/tagsgenerator
+set GOOS=darwin
+set GOARCH=amd64
+go build -o bin/mac64/tagsgenerator
