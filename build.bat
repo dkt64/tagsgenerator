d7f3899e4bc0e9ebaa220737938b3af22ff959a8
DKT <b.apanasewicz@gmail.com>
2021-02-25 20:55:33 +0100

    build update

--- build.bat
+++ build.bat
@@ -1,15 +1,20 @@
 set GOOS=windows
 set GOARCH=amd64
-go build -o bin/win64/tagsgenerator.exe
+go build -o bin/tagsgenerator-win64/tagsgenerator-win64.exe
 set GOOS=windows
 set GOARCH=386
-go build -o bin/win32/tagsgenerator.exe
+go build -o bin/tagsgenerator-win32/tagsgenerator-win32.exe
 set GOOS=linux
 set GOARCH=amd64
-go build -o bin/lin64/tagsgenerator
+go build -o bin/tagsgenerator-linux_amd64/tagsgenerator-linux-amd64
+chmod 0777 bin/tagsgenerator-linux_amd64/tagsgenerator-linux-amd64
 set GOOS=linux
 set GOARCH=arm64
-go build -o bin/arm64/tagsgenerator
+go build -o bin/tagsgenerator-linux_arm64/tagsgenerator-linux-arm64
+chmod 0777 bin/tagsgenerator-linux_arm64/tagsgenerator-linux-arm64
 set GOOS=darwin
 set GOARCH=amd64
-go build -o bin/mac64/tagsgenerator
+go build -o bin/tagsgenerator-mac_amd64/tagsgenerator-mac-amd64
+set GOOS=darwin
+set GOARCH=arm64
+go build -o bin/tagsgenerator-mac_arm64/tagsgenerator-mac-arm64
